ed8b12ef6cdeb321a0f4a0ecefbfb0154b893b32
Ramadan Abdurahman <ramadan1357@gmail.com>
2023-10-24 12:01:58 -0500

    RenderCampsite.js, CampsiteInfoScreen.js

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,28 +1,36 @@
-import { Text, View } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Text, View, StyleSheet } from "react-native";
+import { Card, Icon } from "react-native-elements";
 
 const RenderCampsite = ({ campsite }) => {
-    if (campsite) {
-        return (
-            <Card containerStyle={{ padding: 0 }}>
-                <Card.Image source={campsite.image}>
-                    <View style={{ justifyContent: 'center', flex: 1 }}>
-                        <Text
-                            style={{
-                                color: 'white',
-                                textAlign: 'center',
-                                fontSize: 20
-                            }}
-                        >
-                            {campsite.name}
-                        </Text>
-                    </View>
-                </Card.Image>
-                <Text style={{ margin: 20 }}>{campsite.description}</Text>
-            </Card>
-        );
-    }
-    return <View />;
+  if (campsite) {
+    return (
+      <Card containerStyle={styles.cardContainer}>
+        <Card.Image source={campsite.image}>
+          <View style={{ justifyContent: "center", flex: 1 }}>
+            <Text
+              style={{
+                color: "white",
+                textAlign: "center",
+                fontSize: 20,
+              }}
+            >
+              {campsite.name}
+            </Text>
+          </View>
+        </Card.Image>
+        <Text style={{ margin: 20 }}>{campsite.description}</Text>
+        <Icon name="heart-o" type="font-awesome" color="#f50" raised reverse />
+      </Card>
+    );
+  }
+  return <View />;
 };
+const styles = StyleSheet.create({
+  cardContainer: {
+    padding: 0,
+    margin: 0,
+    marginBottom: 20,
+  },
+});
 
 export default RenderCampsite;

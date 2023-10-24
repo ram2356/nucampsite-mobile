b0674365d444cd229bbecade69891d88764b8def
Ramadan Abdurahman <ramadan1357@gmail.com>
2023-10-24 12:15:41 -0500

    Icons, Favorites, Comments

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,50 +1,57 @@
-import { useState } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
-import RenderCampsite from '../features/campsites/RenderCampsite';
-import { COMMENTS } from '../shared/comments'
+import { useState } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import RenderCampsite from "../features/campsites/RenderCampsite";
+import { COMMENTS } from "../shared/comments";
 
 const CampsiteInfoScreen = ({ route }) => {
-    const [comments, setComments] = useState(COMMENTS)
-    const { campsite } = route.params;
-    const renderCommentItem = ({ item }) => {
-        return (
-            <View style={styles.commentItem}>
-                <Text style={{fontsize: 14}}>{item.text}</Text>
-                <Text style={{fontsize: 12}}>{item.rating} Stars</Text>
-                <Text style={{fontsize: 12}}>{`-- ${item.author}, ${item.date}`}</Text>
-            </View>
-        )
-    }
+  const [comments, setComments] = useState(COMMENTS);
+  const [favorite, setFavorite] = useState(false);
+  const { campsite } = route.params;
+  const renderCommentItem = ({ item }) => {
     return (
-        <FlatList
-          data={comments.filter((comment) => comment.campsiteId === campsite.id)}
-          renderItem={renderCommentItem}
-          keyExtractor={(item) => item.id.toString()}
-          contentContainerStyle={{ marginHorizontal: 20, paddingVertical: 20}}
-          ListHeaderComponent={
-            <>
-              <RenderCampsite campsite={campsite}/>
-              <Text style={styles.commentsTitle}>Comments</Text>
-            </>
-          }
-        />
-    )
+      <View style={styles.commentItem}>
+        <Text style={{ fontsize: 14 }}>{item.text}</Text>
+        <Text style={{ fontsize: 12 }}>{item.rating} Stars</Text>
+        <Text
+          style={{ fontsize: 12 }}
+        >{`-- ${item.author}, ${item.date}`}</Text>
+      </View>
+    );
+  };
+  return (
+    <FlatList
+      data={comments.filter((comment) => comment.campsiteId === campsite.id)}
+      renderItem={renderCommentItem}
+      keyExtractor={(item) => item.id.toString()}
+      contentContainerStyle={{ marginHorizontal: 20, paddingVertical: 20 }}
+      ListHeaderComponent={
+        <>
+          <RenderCampsite
+            campsite={campsite}
+            isFavorite={favorite}
+            markFavorite={() => setFavorite(true)}
+          />
+          <Text style={styles.commentsTitle}>Comments</Text>
+        </>
+      }
+    />
+  );
 };
 const styles = StyleSheet.create({
-    commentsTitle: {
-        textAlign: 'center',
-        backgroundColor: '#fff',
-        fontSize: 16,
-        fontWeight: 'bold',
-        color: '#43484D',
-        padding: 10,
-        paddingTop: 30
-    },
-    commentItem: {
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        backgroundColor: '#fff'
-    }
+  commentsTitle: {
+    textAlign: "center",
+    backgroundColor: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#43484D",
+    padding: 10,
+    paddingTop: 30,
+  },
+  commentItem: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
+  },
 });
 
 export default CampsiteInfoScreen;

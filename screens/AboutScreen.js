41210df8600d72e970de0308c6dee5d2ed8bbf35
Ramadan Abdurahman <ramadan1357@gmail.com>
2023-10-31 11:51:44 -0500

    Gestures Part 1

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -29,30 +29,26 @@ const AboutScreen = () => {
   if (partners.isLoading) {
     return (
       <ScrollView>
-        <Animatable.View
-          animation='fadeInDown'
-          duration={2000}
-          delay={1000}
-        >
           <Mission />
           <Card>
             <Card.Title>Community Partners</Card.Title>
             <Card.Divider />
             <Loading />
           </Card>
-        </Animatable.View>
       </ScrollView>
     );
   }
   if (partners.errMess) {
     return (
       <ScrollView>
-        <Mission />
-        <Card>
-          <Card.Title>Community Partners</Card.Title>
-          <Card.Divider />
-          <Text>{partners.errMess}</Text>
-        </Card>
+        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+          <Mission />
+          <Card>
+            <Card.Title>Community Partners</Card.Title>
+            <Card.Divider />
+            <Text>{partners.errMess}</Text>
+          </Card>
+        </Animatable.View>
       </ScrollView>
     );
   }

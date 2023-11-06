11798f09b8269f14e3171ef799f33199b6e52570
Ramadan Abdurahman <ramadan1357@gmail.com>
2023-11-06 10:33:31 -0600

    LoginScreen.js

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -22,6 +22,7 @@ import AboutScreen from "./AboutScreen";
 import ContactScreen from "./ContactScreen";
 import ReservationScreen from "./ReservationScreen";
 import FavoritesScreen from "./FavoritesScreen";
+import LoginScreen from "./LoginScreen";
 
 const Drawer = createDrawerNavigator();
 
@@ -173,6 +174,27 @@ const FavoritesNavigator = () => {
     </Stack.Navigator>
   );
 };
+const LoginNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={({ navigation }) => ({
+          headerLeft: () => (
+            <Icon
+              name="sign-in"
+              type="font-awesome"
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+            />
+          ),
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
 
 const CustomDrawerContent = (props) => (
   <DrawerContentScrollView {...props}>
@@ -209,6 +231,21 @@ const Main = () => {
         drawerStyle={{ backgroundColor: "#CEC8FF" }}
         drawerContent={CustomDrawerContent}
       >
+        <Drawer.Screen
+          name="Login"
+          component={LoginNavigator}
+          options={{
+            drawerIcon: ({ color }) => (
+              <Icon
+                name="sign-in"
+                type="font-awesome"
+                size={24}
+                iconStyle={{ width: 24 }}
+                color={color}
+              />
+            ),
+          }}
+        />
         <Drawer.Screen
           name="Home"
           component={HomeNavigator}

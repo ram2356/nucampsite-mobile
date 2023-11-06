8047be1285f184fa696aa8c188e9258a53d751e4
Ramadan Abdurahman <ramadan1357@gmail.com>
2023-11-06 11:23:35 -0600

    ReservationScreen.js

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -12,6 +12,7 @@ import {
 import { Picker } from "@react-native-picker/picker";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import * as Animatable from "react-native-animatable";
+import * as Notifications from "expo-notifications";
 
 const ReservationScreen = () => {
   const [campers, setCampers] = useState(1);
@@ -40,7 +41,12 @@ const ReservationScreen = () => {
         },
         {
           text: "Ok",
-          onPress: () => resetForm(),
+          onPress: () => {
+            presentLocalNotification(
+              date.toLocaleDateString("en-US")
+            );
+            resetForm();
+          }
         },
       ],
       { cancelable: false }
@@ -52,6 +58,34 @@ const ReservationScreen = () => {
     setHikeIn(false);
     setDate(new Date());
     setShowCalendar(false);
+  };
+
+  const presentLocalNotification = async (reservationDate) => {
+    const sendNotification = () => {
+      Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+          shouldShowAlert: true,
+          shouldPlaySound: true,
+          shouldSetBadge: true,
+        }),
+      });
+      Notifications.scheduleNotificationAsync({
+        content: {
+          title: 'Your Campsite Reservation Search',
+          body: `Search for ${reservationDate} requested`,
+        },
+        trigger: null
+      });
+    }
+    let permissions = await Notifications.getPermissionsAsync();
+    
+    if (!permissions.granted) {
+      permissions = await Notifications.requestPermissionsAsync();
+    }
+
+    if (permissions.granted) {
+      sendNotification();
+    }
   };
 
   return (
